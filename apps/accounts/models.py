080b89c14b272cbe66afe4502f0cb7319d00a87f
Barry Huffman <barry.a.huffman@gmail.com>
2018-02-23 20:37:28 -0500

    minor template update

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -78,7 +78,7 @@ class User(AbstractBaseUser):
         return self.email
 
     def __str__(self):              # __unicode__ on Python 2
-        return self.email
+        return self.fullname
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
