581a829c3ae33240fadef6754309394089e27696
Barry Huffman <barry.a.huffman@gmail.com>
2018-04-03 09:06:51 -0400

    update local database and removed default from git

--- portal/settings/production.py
+++ portal/settings/production.py
@@ -11,7 +11,7 @@ AUTH_USER_MODEL = 'accounts.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'portal.sqlite3'),
     }
 }
 
